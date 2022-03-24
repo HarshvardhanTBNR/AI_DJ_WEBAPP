8b4d183a04fbece53ca2dfb88a0cac8657d74a2f
HarshvardhanTBNR <76466857+HarshvardhanTBNR@users.noreply.github.com>
2022-03-24 11:38:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+
+function setup(){
+video=createCapture(VIDEO);
+canvas=createCanvas(300,300);
+canvas.center();
+video.size(300,300)
+}
+
+function draw(){
+background("#29f05e")
+}
